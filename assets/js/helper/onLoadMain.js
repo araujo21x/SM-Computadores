e7f5c6b04f08a42b974e6785181199d7233dae06
Lucas <araujolucas97@gmail.com>
2021-01-08 20:29:10 -0300

    side menu

--- assets/js/helper/onLoadMain.js
+++ assets/js/helper/onLoadMain.js
@@ -1,5 +1,5 @@
 const mainLoaded = () => {
+  setBuildingPC({ motherBoard: 1, cpu: 0, ram: null, cooler: null, fonte: null })
   let titleTabs = Array.from(document.getElementsByClassName("titleTab"))
   disableTab(titleTabs)
-  setBuildingPC({ motherBoard: 1, cpu: 0, ram: null, cooler: null, fonte: null })
 }
