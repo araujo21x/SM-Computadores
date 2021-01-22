6e9acce21d36d6c517a0df862ad0d8f282498727
Lucas de Araujo <araujolucas97@outlook.com>
2021-01-22 13:33:31 -0300

    check parts on the motherboard

--- assets/js/helper/checkCompatibility.js
+++ assets/js/helper/checkCompatibility.js
@@ -11,11 +11,11 @@ const checkCompatibility = (piece) => {
    case 'cooler':
       answer = verifyCooler(piece);
       break;
+   case 'ram':
+      verifyRam(piece);
+      break;
    case 'pciExpress':
       answer = verifyPciExpress(piece);
-      break;
-   case 'nvme':
-      answer = verifyNvme(piece);
       break;
    case 'rom':
       answer = verifyRom(piece);
@@ -30,35 +30,113 @@ const checkCompatibility = (piece) => {
    return answer;
 };
 
-const verifyMotherBoard = (piece) =>{
-
+const verifyMotherBoard = (piece) => {
+   return 'compatible';
 };
 
-const verifyCpu = (piece) =>{
+const verifyCpu = (piece) => {
+   const {motherBoard: {socket, chipset}} = getBuildingPC();
+   let answer = 'compatible';
 
+   if (socket !== piece.socket || chipset !== piece.chipset) {
+      answer = 'incompatible';
+   };
+
+   return answer;
 };
 
-const verifyCooler = (piece) =>{
-
+const verifyCooler = (piece) => {
+   return 'compatible';
 };
 
-const verifyPciExpress = (piece) =>{
+const verifyRam = (piece) => {
+   const {motherBoard:
+      {memorySlotAmount, memorySlotType, memorySlotFrequency},
+   } = getBuildingPC();
+   let answer = 'compatible';
 
+   if (memorySlotAmount === 0 || memorySlotType !== piece.memorySlotType) {
+      answer = 'incompatible';
+   } else if (!memorySlotFrequency.include(piece.memorySlotFrequency)) {
+      answer = 'malfunction';
+   }
+
+   return answer;
 };
 
-const verifyNvme = (piece) =>{
+const verifyPciExpress = (piece) => {
+   const {motherBoard: {socketPCIE}} = getBuildingPC();
+   const verifyType = socketPCIE.filter((element) => {
+      return element.type === piece.PCIeType;
+   });
+   // aqui ela verifica se tem uma entrada PCIe do mesmo tipo ex: x16, x2
+   if (verifyType.length === 0) {
+      return 'incompatible';
+   }
 
+   // aqui ela verifica se tem uma versão de PCIe da mesma expelo: PCIe 2.0, 3.0
+   const verifyVersion = verifyType.filter((element) => {
+      return element.version === piece.PCIeVersion;
+   });
+
+   if (verifyVersion.length === 0) {
+      return 'malfunction';
+   } else {
+      return 'compatible';
+   }
 };
 
-const verifyRom = (piece) =>{
+const verifyRom = (piece) => {
+   let answer;
+   switch (piece.typeSocket) {
+   case 'M2':
+      answer = verifyRomM2(piece);
+      break;
+   case 'SATA':
+      answer = verifyRomSATA(piece);
+      break;
+   }
 
+   return answer;
 };
 
-const verifyRecorder = (piece) =>{
-
+const verifyRecorder = (piece) => {
+   return 'compatible';
 };
 
-const verifyPowerSupply = (piece) =>{
+const verifyPowerSupply = (piece) => {
+   const {energy} = getBuildingPC();
+   let answer = 'compatible';
 
+   if (energy > piece.outputPower) {
+      answer = 'incompatible';
+   }
+
+   if (energy === piece.outputPower) {
+      answer = 'malfunction';
+   }
+
+   return answer;
 };
 
+const verifyRomM2 = (piece) => {
+   const {motherBoard: {hasSocketM2, socketM2}} = getBuildingPC();
+
+   if (!hasSocketM2) {
+      return 'incompatible';
+   };
+
+   const verifyTypeSockerM2 = socketM2.filter((element) => {
+      return element.type.include(piece.typeM2);
+   });
+
+   if (verifyTypeSockerM2.length === 0) {
+      return 'incompatible';
+   }
+
+   return 'compatible';
+};
+
+const verifyRomSATA = (piece) => {
+   return 'compatible';
+};
--- assets/js/helper/onLoadMain.js
+++ assets/js/helper/onLoadMain.js
@@ -2,7 +2,47 @@
 
 const mainLoaded = () => {
    setBuildingPC(
-      {motherBoard: 1, cpu: 0, ram: null, cooler: null, fonte: null},
+      {
+         motherBoard: {
+            socket: true,
+            chipset: true,
+            memorySlotAmount: 2,
+            memorySlotType: 'DDR4',
+            memorySlotFrequency: [2666, 2400, 2133],
+            hasSocketM2: false,
+            socketM2: [
+               {type: ['SATA', 'PCIe']},
+               {type: ['PCIe']},
+            ],
+            socketPCIE: [
+               {
+                  version: '3.0',
+                  type: 'x16',
+               },
+            ],
+         },
+         cpu: 0,
+         ram: null,
+         rom: {
+            name: '',
+            size: '',
+            typeSocket: 'M2',
+            typeM2: 'SATA',
+            typeSATA: 'SSD',
+         },
+         placaVideo: {
+            type: 'pciExpress',
+            subType: 'graphicsCard',
+            PCIeVersion: '3.0',
+            PCIeType: 'x16',
+            EnergyConsumption: 300,
+         },
+         cooler: null,
+         powerSupply: {
+            outputPower: 500,
+         },
+         energy: 450,
+      },
    );
    const titleTabs = Array.from(document.getElementsByClassName('titleTab'));
    disableTab(titleTabs);
